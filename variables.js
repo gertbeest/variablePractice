52c666de46c701bca69496ac7cd62246c360d733
Alex Alexander <a.alexander62442@gmail.com>
2022-11-08 18:52:31 -0700

    Reassign variables, add console.log

--- variables.js
+++ variables.js
@@ -1,9 +1,24 @@
 var email = "catwhiskers9@msn.com"
 var book = "dune"
 var song = "strawberry swing"
+
+console.log(email)
+
+email = "a.alexander62442@gmail.com"
+book = "messiah"
+song = "nightwalk"
+
 var age = 31
 var speed = 60
 var numBudgies = 2
+
+speed = 100
+console.log(speed)
+console.log("myNameIsAlex")
+
 var isPercyBlue = true
 var isLemonBlue = false
 var isLemonYellow = true
+
+isPercyBlue = false
+console.log(isPercyBlue)
